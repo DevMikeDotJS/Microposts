53b208050ffb687cbdf459392e94c6f7d399292c
Mike Rivera <michaelstephenrivera@gmail.com>
2020-01-24 15:46:01 -0800

    Add Posts and Show Alert

--- src/ui.js
+++ src/ui.js
@@ -2,7 +2,7 @@ class UI {
   constructor() {
     this.post = document.querySelector('#posts');
     this.titleInput = document.querySelector('#title');
-    this.bodyInput = document.querySelector('#title');
+    this.bodyInput = document.querySelector('#body');
     this.idInput = document.querySelector('#id');
     this.postSubmit = document.querySelector('.post-submit');
     this.forState = 'add';
@@ -29,6 +29,41 @@ class UI {
     });
 
     this.post.innerHTML = output;
+  }
+
+  showAlert(message, className) {
+    this.clearAlert();
+
+    // Create div
+    const div = document.createElement('div');
+    // Add classes
+    div.className = className;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const container = document.querySelector('.postsContainer');
+    // Get posts
+    const posts = document.querySelector('#posts');
+    // Insert alert div
+    container.insertBefore(div, posts);
+
+    // Timeout
+    setTimeout(() => {
+      this.clearAlert();
+    }, 3000);
+  }
+
+  clearAlert() {
+    const currentAlert = document.querySelector('.alert');
+
+    if (currentAlert) {
+      currentAlert.remove();
+    }
+  }
+
+  clearFields() {
+    this.titleInput.value = '';
+    this.bodyInput.value = '';
   }
 }
 
